307b2fa9afb1633c2a4e9ff564a2648ad4a7d51a
miguelcamposfernandes <miguelfernandes.personal@gmail.com>
2021-06-29 16:27:42 +0200

    Fixing Internal Server Error, Changing Name, Adding Logo

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, request, url_for, redirect, send_file, session
 from pytube import YouTube
+from io import BytesIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "654c0fb3968af9d5e6a9b3edcbc7051b"
-
-path = "C:\dev\Projects\Youtube Video Downloader\downloads"
 
 @app.route("/", methods = ["GET", "POST"])
 def home():
@@ -21,11 +20,13 @@ def home():
 @app.route("/download", methods = ["GET", "POST"])
 def download_video():
     if request.method == "POST":
+        buffer = BytesIO()
         url = YouTube(session['link'])
         itag = request.form.get("itag")
         video = url.streams.get_by_itag(itag)
-        file = video.download(path)
-        return send_file(file, as_attachment=True)
+        video.stream_to_buffer(buffer)
+        buffer.seek(0)
+        return send_file(buffer, as_attachment=True, download_name="Video - YT2Video.mp4", mimetype="video/mp4")
     return redirect(url_for("home"))
 
 if __name__ == '__main__':
